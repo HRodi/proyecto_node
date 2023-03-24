a926fcc8ddc8faa501a36846d0733b9a01ed5e06
Hider Rodi <hider.david.rodi@gmail.com>
2023-03-24 19:15:04 -0300

    first commit

---
+++ study_app_project/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser());
+
+const userRoute = require("./src/route/users/users.route");
+const themeRoute = require("./src/route/themes/themes.route");
+const topicsRoute = require("./src/route/topics/topics.route");
+ 
+//Ruta raiz
+app.get('/', function (req, res) {
+    //Logica.
+    res.send(`<div style=color:red><h1>Hello World</h1></div>
+                <h2>Hola mundo</h2>`);
+});
+
+app.get('/pagina2', function (req, res) {
+    //Logica de negocios
+    //esta aqui -Controller
+
+    res.json({application: 'Study APP', version: '1.0.0'});
+});
+
+//Llamadas a los routes de los UCs
+userRoute(app);
+themeRoute(app);
+topicsRoute(app);
+
+//puerto en el cual se "escucha"
+app.listen(3000);
